f1fa4f70654b40054c3de1f001f4623f3eab0c1e
pedropeglow <pedropeglowm@gmail.com>
2023-08-21 19:06:42 -0300

    :sparkles: Adicionando função para recuperar cep.

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello World!")
+async function buscaEndereco(cep) {
+    const mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+    try {
+    let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+    let consultaCEPConvertida = await consultaCEP.json();
+    if(consultaCEPConvertida.erro) {
+        throw Error("CEP não existente!")
+    }
+    let cidade = document.getElementById('cidade');
+    let logradouro = document.getElementById('endereco');
+    let estado = document.getElementById('estado');
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+     return consultaCEPConvertida
+    } catch(erro) {
+        mensagemErro.innerHTML = `<p>CEP Inválido. Tente Novamente!</p>`
+    }
+}
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value))
